7629a275b920b104f517a5581e8c8d41dbd2d001
joaquingb4 <joaquin_alonsogarcia@iesjoandaustria.org>
2022-03-14 19:58:02 +0100

    init

--- app/src/main/java/com/example/fragments/Config/DefaultConstants.java
+++ app/src/main/java/com/example/fragments/Config/DefaultConstants.java
@@ -6,8 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class DefaultConstants {
 
     public static final String API_KEY = "b600e6605b78c1e0846ecd4d57e40ae9" /*"9d3d87dfab03093a4a78ce77f8b3072b"*/;
-    public static final String SESSION_ID = "919a1e6fca739322967249671e19562dfb9bb689";
-    public static final String ACCOUNT_ID = "martamill3";
+    public static final String SESSION_ID = "bf49e752495a327e2f4ad9b555edadacc307da6d"/*"919a1e6fca739322967249671e19562dfb9bb689"*/;
+    public static final String ACCOUNT_ID = "alumno21";
 
     public static final String BASE_IMG_URL = "https://image.tmdb.org/t/p/w500/";
 
@@ -25,6 +25,10 @@ public class DefaultConstants {
      */
         //Token request
     /*cbe62a471984457d40c018a17cb445986d3703c6*/
+
+    //73c6958f499f8dec1e032f72d62ee4462c1265d9
+    //https://www.themoviedb.org/authenticate/73c6958f499f8dec1e032f72d62ee4462c1265d9
+        //sesionId : bf49e752495a327e2f4ad9b555edadacc307da6d
 
 
 }
